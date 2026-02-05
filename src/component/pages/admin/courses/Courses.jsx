209861f82d3e4d2017f4f12156d5488c8c021c19
Jeetu rajput <jeeturajput0302@gmail.com>
2026-02-05 14:34:10 +0530

    courese.reprot

---
+++ src/component/pages/admin/courses/Courses.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Courses() {
+  return (
+    <div>
+      <h1>kljhjf</h1>
+    </div>
+  )
+}
---
+++ src/component/pages/admin/report/Report.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Report() {
+  return (
+    <div>
+      <h1>jkjhhgf</h1>
+    </div>
+  )
+}
